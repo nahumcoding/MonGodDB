ab4a877f0b9f2a08e9103321fea0ef2d02fa33f6
nahum sonck <33633504+nahumcoding@users.noreply.github.com>
2018-05-27 17:00:21 -0400

    git commit -m

--- server.js
+++ server.js
@@ -14,10 +14,17 @@ var request = require("request");
 var cheerio = require("cheerio");
 
 // Set mongoose to leverage built in JavaScript ES6 Promises
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
 mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+
 
 //Define port
-var port = process.env.PORT || 3000
+var port = process.env.PORT || 27017
 
 // Initialize Express
 var app = express();
